5d19f6c956728ffb9d7bd3670a13c077f35e2164
inancKocakaya <inanc.kocakaya@gmail.com>
2025-04-15 19:40:13 +0200

    Configuration for protobuf object exchange

---
+++ src/main/java/com/kocakaya/pockafka/configuration/KafkaConfiguration.java
@@ -0,0 +1,68 @@
+package com.kocakaya.pockafka.configuration;
+
+import com.kocakaya.pockafka.HelloWorldResponse;
+import com.kocakaya.pockafka.serialization.HelloWorldDeserializer;
+import com.kocakaya.pockafka.serialization.HelloWorldSerializer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaProducerConfiguration {
+
+    @Bean
+    public ConsumerFactory<String, HelloWorldResponse> consumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(
+                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                "http://localhost:9092");
+        props.put(
+                ConsumerConfig.GROUP_ID_CONFIG,
+                "ink");
+        props.put(
+                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class);
+        props.put(
+                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                HelloWorldDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, HelloWorldResponse>
+    kafkaListenerContainerFactory() {
+
+        ConcurrentKafkaListenerContainerFactory<String, HelloWorldResponse> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+
+    @Bean
+    public ProducerFactory<String, HelloWorldResponse> greetingProducerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(
+                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                "http://localhost:9092");
+        props.put(
+                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class);
+        props.put(
+                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                HelloWorldSerializer.class);
+        return new DefaultKafkaProducerFactory<>(props);
+    }
+
+    @Bean
+    public KafkaTemplate<String, HelloWorldResponse> greetingKafkaTemplate() {
+        return new KafkaTemplate<>(greetingProducerFactory());
+    }
+}
---
+++ src/main/java/com/kocakaya/pockafka/serialization/HelloWorldDeserializer.java
@@ -0,0 +1,4 @@
+package com.kocakaya.pockafka.serialization;
+
+public class HelloWorldDeserializer {
+}
